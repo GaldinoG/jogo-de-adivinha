3bd1f395099c604d7e90756cc911b7fb11e74eae
Gabriel Galdino De Oliveira <1000576@PPC01INFLL00314.pucminas.net>
2019-02-18 20:13:58 -0300

    primeira versao do jogo

--- exercicio1.rb
+++ exercicio1.rb
@@ -3,8 +3,21 @@ number = 7
 puts("Digite seu nome: ")
 nome = gets
 
-puts("Digite o valor " + nome)
-chute = gets.chomp.to_i
+i = 1
+while i <= 3
+  puts("\nDigite o valor " + nome)
+  chute = gets.chomp.to_i
+  if chute == number
+    abort("Acertou!!")
+  else
+    if chute > number
+      puts "O numero secreto é menor \n\n"
+    else
+      puts "O numero secreto é maior \n\n"
+    end
+  end 
+  i += 1
+end
 
-puts chute == number
+abort("Fim do jogo")
 
