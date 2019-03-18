8eccfc85e162ce0cc851eda82eba69b24c2e01cc
Gabriel Galdino De Oliveira <1000576@PPC01INFLL00328.pucminas.net>
2019-03-18 19:23:37 -0300

    primeira versao do jogo

--- exercicio1.rb
+++ exercicio1.rb
@@ -1,10 +1,26 @@
-def jogoAdivinha(nome)
-  number = 7
+def jogoAdivinha(nome, listArray)
+  
+  puts("\nEscolha seu nivel de dificuldade: 1 - 2 - 3 - 4 - 5")
+  strNivel = gets.chomp.to_i
+  
+  case strNivel
+  when 1
+    number = rand(0..30)
+  when 2
+    number = rand(0..60)
+  when 3
+    number = rand(0..100)
+  when 4
+	number = rand(0..150)
+  when 5
+    number = rand(0..200)
+  end
 
   i = 1
   while i <= 3
     puts("\nDigite o valor " + nome)
     chute = gets.chomp.to_i
+	listArray.push(chute)
     if chute == number
       abort("Acertou!!")
     else
@@ -19,28 +35,30 @@ def jogoAdivinha(nome)
 
 end
 
-def jogarNovamente(nome)
+def jogarNovamente(nome, listArray)
   puts("\n\n" + nome + "você deseja jogar novamente? 1-Sim 2-Não")
   strBack = gets.chomp.to_i
-
+  
   if strBack == 1
     system("clear")
-    jogoAdivinha(nome)
+    puts("\nNumeros que ja foram chutados:" + listArray.inspect)
+
+    jogoAdivinha(nome, listArray)
   elsif strBack == 2
     abort("Fim do jogo")  
   else
     puts("\n\nValor invalido")
-    jogarNovamente(nome)
+    jogarNovamente(nome, listArray)
   end 
 
   if strBack == 1
-    jogarNovamente(nome)
+    jogarNovamente(nome, listArray)
   end
 end
 
-
+listArray = Array.new
 puts("Digite seu nome: ")
 nome = gets
-jogoAdivinha(nome)
-jogarNovamente(nome)
+jogoAdivinha(nome, listArray)
+jogarNovamente(nome, listArray)
 
